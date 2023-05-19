ee43e71e959d0b748aad024c822b16c920794b28
sam arman <97samarman@gmail.com>
2023-05-19 13:13:30 +0100

    added views and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   # List of all restaurants
-
   resources :restaurants do
     resources :reviews, only: [:new, :create]
   end
 
-  root 'restaurants#index'
   # Add a new restaurant
   get 'restaurants/new', to: 'restaurants#new'
   post 'restaurants', to: 'restaurants#create'
 
-  # Details of a restaurant with reviews
-  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
-
   # Add a new review to a restaurant
   get 'restaurants/:id/reviews/new', to: 'reviews#new'
   post 'restaurants/:id/reviews', to: 'reviews#create'
+
+  root 'restaurants#index'
 end
